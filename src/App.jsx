1eb5d07b06d34a69ce438b6bef665726cb2994b2
CaioMantovaniBorba <caiovmborba@gmail.com>
2023-06-07 16:19:48 -0300

    Add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import { useState, useEffect } from 'react'
+import qs from 'qs';
 
 import SearchInput from './components/SearchInput';
+import Pagination from './Pagination';
 
 const api = 'https://kitsu.io/api/edge/';
+
+const LIMIT = 12;
 
 function App() {
   const [info, setInfo] = useState({});
   const [text, setText] = useState('');
+  const [offset, setOffset] = useState([0]);
 
   useEffect(() => {
-    if (text) {
-      setInfo({});
-      fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
-        .then((response) => response.json())
-        .then((response) => setInfo(response));
+    const query = {
+      page: {
+        limit: LIMIT,
+        offset,
       }
-  }, [text]);
+    };
+
+    if (text) {
+      query.filter = {
+        text,
+      };
+    }
+
+    fetch(`${api}anime?${qs.stringify(query)}`)
+      .then((response) => response.json())
+      .then((response) => setInfo(response));
+
+  }, [text, offset]);
 
   return (
     <div className="container">
-      <SearchInput value={text} onChange={(search) => setText(search)}/>
+      <SearchInput value={text} onChange={(search) => setText(search)} />
 
       {text && !info.data && (
         <span className="loader"></span>
@@ -28,11 +44,20 @@ function App() {
       {info.data && (
         <ul>
           {info.data.map(item =>
-          <li key={item.id}>
-            <img src={item.attributes.posterImage.small} alt="Anime image" />
-            {item.attributes.canonicalTitle}
-          </li>)}
+            <li key={item.id}>
+              <img src={item.attributes.posterImage.small} alt="Anime image" />
+              {item.attributes.canonicalTitle}
+            </li>)}
         </ul>
+      )}
+
+      {info.meta && (
+        <Pagination
+          limit={LIMIT}
+          total={info.meta.count}
+          offset={offset}
+          setOffset={setOffset}
+        />
       )}
     </div>
   )
